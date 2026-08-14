ee052cb9dd7493788ac45d19ce8cfa0768a475af
vrajlimbani-wq <vrajlimbani@gmail.com>
2026-08-14 19:33:04 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,20 @@
+import 'package:flutter/material.dart';
+import 'profile_switcher.dart';
+
 void main() {
-  print("Star India App Initialized");
+  runApp(StarIndiaApp());
+}
+
+class StarIndiaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Star India',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: ProfileSwitcherScreen(),
+    );
+  }
 }
