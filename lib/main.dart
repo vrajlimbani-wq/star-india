00e8dd0f00d5b73a5290daefc42d24a918111974
vrajlimbani-wq <vrajlimbani@gmail.com>
2026-08-14 19:53:09 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'profile_switcher.dart';
+import 'feed_screen.dart';
+import 'chat_screen.dart';
 
 void main() {
   runApp(StarIndiaApp());
@@ -14,7 +16,51 @@ class StarIndiaApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: ProfileSwitcherScreen(),
+      home: MainDashboard(),
+    );
+  }
+}
+
+class MainDashboard extends StatefulWidget {
+  @override
+  _MainDashboardState createState() => _MainDashboardState();
+}
+
+class _MainDashboardState extends State<MainDashboard> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    FeedScreen(),
+    ChatScreen(),
+    ProfileSwitcherScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Feed',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
     );
   }
 }
