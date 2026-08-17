b6e5bf77618f16a7d3f3a39d6c84357443f4dd8a
vrajlimbani-wq <vrajlimbani@gmail.com>
2026-08-17 13:38:12 +0530

    Update profile_model.dart

--- lib/lib/models/profile_model.dart
+++ lib/lib/models/profile_model.dart
@@ -10,5 +10,22 @@ class UserProfile {
     required this.profileType,
     required this.avatarUrl,
   });
-}
 
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'profileType': profileType,
+      'avatarUrl': avatarUrl,
+    };
+  }
+
+  factory UserProfile.fromMap(Map<String, dynamic> map, String docId) {
+    return UserProfile(
+      id: docId,
+      name: map['name'] ?? '',
+      profileType: map['profileType'] ?? 'Personal',
+      avatarUrl: map['avatarUrl'] ?? '',
+    );
+  }
+}
